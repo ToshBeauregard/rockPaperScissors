2561acfc77130c9ce7fafa6d935a61971dc2db26
ToshBeauregard <42378630+ToshBeauregard@users.noreply.github.com>
2018-08-14 21:37:10 +0900

    Add files via upload

---
+++ rockScissorsPaper.sol
@@ -0,0 +1,34 @@
+//THIS CODE CONSUME TOO MUCH GAS
+//NOT FOR USE 
+pragma solidity ^0.4.16;
+
+contract rockScissorsPaper {
+
+    function battle(string) public payable returns (string){
+        uint randNum; //randNum is a unsigned integer
+        randNum = uint(keccak256(abi.encode(now))) % 3; //Generating random number between 0,1,2
+        require (msg.value == 1 ether);
+        
+        if (randNum == 0){
+            
+            msg.sender.transfer(this.balance);
+            return  "You win";
+
+        } else if (randNum == 1){
+            return  "You lose";
+
+        } else {
+            msg.sender.transfer(1 ether);
+            return  "You even";
+           
+            
+        }
+        
+    }
+    
+    function getDeposit ()   returns (uint){ //Get how much ether is deposited on this contract
+        return (this.balance)/1000000000000000000; //Display the deposit 
+    }
+
+    
+}
